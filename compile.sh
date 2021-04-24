8fe8ef91f94b64a735498b7e864dd0f02d69c226
youngchief btw ツ <youngchief@youngchief.tk>
2021-04-24 20:40:45 +0000

    Add script to compile everything.

---
+++ compile.sh
@@ -0,0 +1,4 @@
+g++ linux_client_ipv4.cpp -o linux_client_ipv4;
+g++ linux_client_ipv6.cpp -o linux_client_ipv6;
+g++ linux_server_ipv4.cpp -o linux_server_ipv4;
+g++ linux_server_ipv6.cpp -o linux_server_ipv6;
